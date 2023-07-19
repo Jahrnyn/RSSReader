898419d99ba0021e2f3860e21560e4b32052bd2c
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-07-19 15:24:23 +0200

    added new Model for subscriptions and working on RssController

--- app/Http/Controllers/RssController.php
+++ app/Http/Controllers/RssController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\RssSubscription;
 use Illuminate\Validation\Rule;
 
 class RssController extends Controller
--- app/Models/RssSubscription.php
+++ app/Models/RssSubscription.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RssSubscription extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'url'];
+    
+    // Define the relationship with the User model
+    public function user(){
+        return $this->belongsTo(User::class);
+    }        
+
 }
