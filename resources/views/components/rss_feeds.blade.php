f7579289332bb3793833462e4fe2865ac8834c4c
Zoltan C <cziljak.zoltan.andras@gmail.com>
2023-07-24 13:26:16 +0200

    RSS Feeds displaying works! (8 last feed displayed in the homepage

--- resources/views/components/rss_feeds.blade.php
+++ resources/views/components/rss_feeds.blade.php
@@ -1,18 +1,36 @@
 {{-- rss_feeds.blade.php --}}
-{{-- rss_feeds.blade.php --}}
 @if ($rssData)
     <h3>Selected Subscription Title: {{ $rssData['title'][0] }}</h3>
 
+    @php
+        $counter = 0;
+    @endphp
+    
     @foreach ($rssData['items'] as $item)
-        <h3>{{ is_array($item['itemTitle']) ? $item['itemTitle'][0] : $item['itemTitle'] }}</h3>
+        @php
+            $itemTitle = is_array($item['itemTitle']) ? $item['itemTitle'][0] : $item['itemTitle'];
+            $itemDescription = is_array($item['itemDescription']) ? $item['itemDescription'][0] : $item['itemDescription'];
+            $itemPubDate = is_array($item['itemPubDate']) ? $item['itemPubDate'][0] : $item['itemPubDate'];
+        @endphp
 
-        @if (!is_array($item['itemDescription']))
-            <p>{!! $item['itemDescription'] !!}</p>
+        <h3>{{ $itemTitle }}</h3>
+
+        {{-- Handle 'itemDescription' --}}
+        @if (is_array($itemDescription))
+            <pre>{{ print_r($itemDescription, true) }}</pre>
         @else
-            <p>{!! $item['itemDescription'][0] !!}</p> 
+            <p>{!! htmlspecialchars_decode($itemDescription) !!}</p>
         @endif
 
-        <p>{{ is_array($item['itemPubDate']) ? $item['itemPubDate'][0] : $item['itemPubDate'] }}</p>
+        <p>{{ $itemPubDate }}</p>
+
+        {{-- displaying only 8 feeds in the view --}}
+        @php
+            $counter++;
+            if ($counter === 8) {
+                break;
+            }
+        @endphp
     @endforeach
 
 @else
